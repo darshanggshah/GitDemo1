90b791e721f09c72983a67052cfca3ae42e0cedf
Darshan <darshanggshah@gmail.com>
2020-08-27 13:24:29 -0400

    first commit1

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test 
+	public void NativAppAndroid()
+	{
+		System.out.println("NativAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		
+		System.out.println("IOSApp");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	
+	@Test 
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwiter()
+	{
+		System.out.println("deleteTwiter");
+		
+	}
+}
